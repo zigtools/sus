8c7c6925f7d40a2d137ad0a9f784c2c24ae4265e
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2022-12-18 04:27:15 -0500

    Basic server done

--- server/src/index.js
+++ server/src/index.js
@@ -6,21 +6,30 @@ const app = express();
 
 const savedLogsPath = path.join(__dirname, "../..", "saved_logs");
 
+function getLogs() {
+    return fs.readdirSync(savedLogsPath).map(_ => ({
+        name: _,
+        date: new Date(+_.split("-")[1]),
+    })).sort((a, b) => b.date - a.date);
+}
+
 app.get("/", (req, res) => {
+    console.log(getLogs());
     res.render("index.ejs", {
-        logs: fs.readdirSync(savedLogsPath),
+        logs: getLogs(),
     });
 });
 
-app.get("/log/:log", (req, res) => {
+app.get("/log/:log/:kind", (req, res) => {
     // Prevent OWASP / \ injection attack (is there a safer way to do this?)
-    const logDir = path.join(savedLogsPath, path.basename(req.params.log));
+    const log = path.basename(req.params.log);
+    const logDir = path.join(savedLogsPath, log);
+    const kind = req.params.kind;
 
-    if (!fs.existsSync(logDir)) return res.status(404).end("404");
+    if (!fs.existsSync(logDir) || !["stderr", "stdin", "stdout"].includes(kind)) return res.status(404).end("404");
 
-    res.render("log.ejs", {
-        logs: fs.readdirSync(savedLogsPath),
-    });
+    res.contentType("text");
+    res.end(fs.readFileSync(path.join(logDir, kind + ".log")).toString());
 });
 
 app.listen(80);
