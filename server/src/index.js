2031388c21a724968fc28a0119eacf5b99afe04b
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2022-12-18 05:19:12 -0500

    Add relation mapping; TODO make it dynamic

--- server/src/index.js
+++ server/src/index.js
@@ -4,19 +4,73 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("static"));
+
 const savedLogsPath = path.join(__dirname, "../..", "saved_logs");
 
-function getLogs() {
-    return fs.readdirSync(savedLogsPath).map(_ => ({
-        name: _,
-        date: new Date(+_.split("-")[1]),
-    })).sort((a, b) => b.date - a.date);
+const panicRegex = /thread \d*? panic: ((?:.|\n)*$)/gm;
+
+function logInfo(log) {
+    return {
+        name: log,
+        date: new Date(+log.split("-")[1]),
+    };
 }
 
+function getLogs() {
+    return fs.readdirSync(savedLogsPath).map(logInfo).sort((a, b) => b.date - a.date);
+}
+
+let relations = [];
+
+function calculateRelations() {
+    const logs = getLogs();
+
+    logScramble: for (const log of logs) {
+        let err = fs.readFileSync(path.join(savedLogsPath, log.name, "stderr.log")).toString();
+        err = err.match(panicRegex)[0].split("\n").slice(1).join("\n");
+
+        for (const relation of relations) {
+            if (relation[0].err == err) {
+                relation.push({
+                    log
+                });
+                continue logScramble;
+            }
+        }
+
+        relations.push([{
+            log,
+            err,
+        }]);
+    }
+}
+
+calculateRelations();
+
 app.get("/", (req, res) => {
-    console.log(getLogs());
     res.render("index.ejs", {
         logs: getLogs(),
+    });
+});
+
+app.get("/log/:log", (req, res) => {
+    const log = path.basename(req.params.log);
+    const logDir = path.join(savedLogsPath, log);
+
+    const err = fs.readFileSync(path.join(logDir, "stderr.log")).toString();
+    const related = relations.find(_ => {
+        for (const p of _) {
+            if (p.log.name === log) return true;
+        }
+        return false;
+    });
+
+    res.render("info.ejs", {
+        logs: getLogs(),
+        log: logInfo(log),
+        err: err.match(panicRegex)[0],
+        related
     });
 });
 
